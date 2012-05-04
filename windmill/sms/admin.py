b8c98f769131b66ecb772bbfe63f10160a918c01
Christian Schaffner <huebli@gmail.com>
2012-05-04 18:23:08 +0200

    starting off the SMS business

---
+++ windmill/sms/admin.py
@@ -0,0 +1,15 @@
+from windmill.sms.models import Tournament, SMS
+from django.contrib import admin
+
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger('windmill.spirit')
+
+
+class SMSAdmin(admin.ModelAdmin):
+    list_display = ['id','team','tournament','round_id','message','status']
+    list_filter = ('tournament','team','round_id')
+
+admin.site.register(Tournament)
+admin.site.register(SMS, SMSAdmin)
