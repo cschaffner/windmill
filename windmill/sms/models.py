9f2bdb9a7dc1664362614d0023c170ad3973c97c
Christian Schaffner <huebli@gmail.com>
2012-05-19 18:54:28 +0200

    finishing work on two servers

--- windmill/sms/models.py
+++ windmill/sms/models.py
@@ -70,8 +70,12 @@ class SMSManager(models.Manager):
         for g in thisRound['games']:
             if g['team_1'] is not None:
                 msg=self.msg_swiss_team(prevRound,thisRound,g['team_1'],g['team_2'],g['start_time'],vp_bye) # TODO: Field
+                if settings.HOST=="playwithlv.com":
+                    team_obj=Team.objects.get(l_id = g['team_1_id'])
+                elif settings.HOST=="leaguevine.com":
+                    team_obj=Team.objects.get(lv_id = g['team_1_id'])
                 sms = SMS.objects.create(message = msg,
-                                         team = Team.objects.get(l_id = g['team_1_id']),
+                                         team = team_obj,
                                          round_id = thisRound['round_number'],
                                          tournament = tourney,
                                          status = 1,
@@ -79,8 +83,12 @@ class SMSManager(models.Manager):
                 nr_created += 1
             if g['team_2'] is not None:
                 msg=self.msg_swiss_team(prevRound,thisRound,g['team_2'],g['team_1'],g['start_time'],vp_bye) # TODO: Field
+                if settings.HOST=="playwithlv.com":
+                    team_obj=Team.objects.get(l_id = g['team_2_id'])
+                elif settings.HOST=="leaguevine.com":
+                    team_obj=Team.objects.get(lv_id = g['team_2_id'])
                 sms = SMS.objects.create(message = msg,
-                                         team = Team.objects.get(l_id = g['team_2_id']),
+                                         team = team_obj,
                                          round_id = thisRound['round_number'],
                                          tournament = tourney,
                                          status = 1,
