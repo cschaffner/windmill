7c26b9a28681398015baaf20d20ac0fe116d06da
Christian Schaffner <huebli@gmail.com>
2012-06-13 10:13:26 +0200

    sms overview

--- windmill/sms/views.py
+++ windmill/sms/views.py
@@ -50,7 +50,7 @@ def status_update(request):
         status = request.GET['STATUS']
     except:
         logger.error('something went wrong while extracting information from GET body')
-        return HttpResponse('<html>status failed</html>')
+        return HttpResponse('<html>sms status update failed</html>')
     else:   
         sms=SMS.objects.get(id=reference)
         sms.status=status
