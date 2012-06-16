10633448e276bf0fbbe11c3d2cbc6fc7f770de28
Christian Schaffner <huebli@gmail.com>
2012-06-16 23:50:22 +0200

    fixed spirit stuff

--- windmill/spirit/views.py
+++ windmill/spirit/views.py
@@ -1,7 +1,7 @@
 from __future__ import division
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.conf import settings
-from windmill.spirit.models import Game
+from windmill.spirit.models import Game, Team
 import logging
 from pprint import pformat
 
@@ -11,6 +11,11 @@ logger = logging.getLogger('windmill.spirit')
 
 def home(request):
     # refer to admin interface 
+    return render_to_response('spirit_added_games.html',{'added': 0})
+
+def update(request):
+    # refer to admin interface 
+    Team.objects.update_all()
     return render_to_response('spirit_added_games.html',{'added': 0})
 
     
